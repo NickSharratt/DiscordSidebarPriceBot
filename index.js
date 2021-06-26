7fe92216bda37827a6acdf8e706fdf1e9c1ebaba
Nikolai828 <nicholasgsharratt@gmail.com>
2021-06-26 20:19:38 +0100

    Price changed to cent and up and down arrows added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const discordBotToken = YOUR BOT TOKEN HERE;
+const discordBotToken = 'YOUR BOT TOKEN BETWEEN SINGLE QUOTATION MARKS';
 
 const api = require('kucoin-node-api');
 
@@ -23,8 +23,6 @@ bot.on('ready', () => {
 
     bot.guilds.cache.each(guild => guildMeCache.push(guild.me));
 
-
-
     let symbol = 'VRA-USDT';
 
     const getPrice = () => {
@@ -33,9 +31,10 @@ bot.on('ready', () => {
             //console.log(result.data.price)
             let price = result.data.last;
             let trimmedPrice = parseFloat(price.substring(0,7));
+	    let centPrice = (trimmedPrice * 100);
             let change = result.data.changePrice;
-            
-            guildMeCache.forEach(guildMe => guildMe.setNickname(`$${trimmedPrice} | ${((change/(-change+trimmedPrice))*100).toFixed(2)}%`));
+	    let changeArrow = change > 0 ? '▲' : (change < 0 ? '▼' : '▶')
+            guildMeCache.forEach(guildMe => guildMe.setNickname(`${centPrice}¢ ${changeArrow} ${((change/(-change+trimmedPrice))*100).toFixed(2)}%`));
         })
             //console.log(output.data.changeRate)
             bot.user.setActivity(`VRA on Kucoin`, { type: 'WATCHING'});
