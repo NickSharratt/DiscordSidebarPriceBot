396571db0c61b7a7c2b3a86c21bac1a36f891ea3
Nikolai828 <nicholasgsharratt@gmail.com>
2021-06-26 19:28:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const api = require('kucoin-node-api')
+
+const config = {
+    apiKey: 'xXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxXXX',
+    secretKey: 'xxxxxxxxXXXXXXXXXXXXXxxXXXXXXXXXXXXXxxxXXX',
+    passphrase: 'xxxxxx',
+    environment: 'live'
+}
+
+api.init(config)
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+let guildMeCache = [];
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+    bot.user.setActivity(`VRA on Kucoin`, { type: 'WATCHING'});
+
+    bot.guilds.cache.each(guild => guildMeCache.push(guild.me));
+
+
+
+    let symbol = 'VRA-USDT';
+
+    const getPrice = () => {
+        let data =  api.get24hrStats(symbol);
+        data.then(function(result) {
+            //console.log(result.data.price)
+            let price = result.data.last;
+            let trimmedPrice = parseFloat(price.substring(0,7));
+            let change = result.data.changePrice;
+            
+            guildMeCache.forEach(guildMe => guildMe.setNickname(`$${trimmedPrice} | ${((change/(-change+trimmedPrice))*100).toFixed(2)}%`));
+        })
+            //console.log(output.data.changeRate)
+            bot.user.setActivity(`VRA on Kucoin`, { type: 'WATCHING'});
+    }
+
+    function myFunction() {
+        myVar = setInterval(getPrice,10000)
+    }
+
+    myFunction()
+})
+
+
+bot.login('ODUzNDA5NzAzNjQwMDM5NDI0.YMU9uA.33Ho8XWoq5fa4I17EsfRIyjbi1o');
